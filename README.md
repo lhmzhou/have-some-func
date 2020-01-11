017e0f477d6781ca6b64d35173dc492f780580e8
lhmzhou <Linda.Zhou@aexp.com>
2020-01-11 08:31:32 -0500

    set up env variables

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # have-some-func
-creating a basic CRUD application 
+
+This Go application demonstrates the use of CRUD operations on a SQLite database for local data.
+
+## Installation 
+
+Install `go-sqlite3` dependency from source
+```
+$ go get github.com/mattn/go-sqlite3
+```
+
+## Run Project
+```
+$ go run src/main.go
+```
+
+## Go Framework Credits
+
+[SQLite driver](https://github.com/mattn/go-sqlite3)
+[Gin](https://github.com/gin-gonic/gin/)
+
+## Other Related Projects
+
+[sqlc](https://github.com/relops/sqlc)
+
+## Contributing
+
+Pull requests are welcomed. Please use project in production at your own peril.
+
---
+++ src/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+type Data struct {
+	ID          int64  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+}
+
+func main() {
+	db, err = sql.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	os.Setenv("PORT", "8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	/*
+		resp, err := http.PostForm("http://localhost:5003/create",
+			url.Values{"ID": {"1234"}, "Name": {"hello"}, " Description" : {"hello Project"}, "URL" : {"www.google.com"}})
+
+
+
+		//resp, err := http.Get("http://localhost:5003/index1")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+
+		// Print the HTTP Status Code and Status Name
+		fmt.Println("HTTP Response Status:", resp.StatusCode, http.StatusText(resp.StatusCode))
+
+		if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+			fmt.Println("HTTP Status is in the 2xx range")
+		} else {
+			fmt.Println("Error ",resp.StatusCode,resp.Status)
+		}
+
+	*/
+
+}
